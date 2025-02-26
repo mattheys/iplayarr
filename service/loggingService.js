76efeb914fd471590fc8f8ce20b7bccf26f64b3b
Jamie Bartlett <nikorag@gmail.com>
2025-02-26 12:16:40 +0000

    Fix logging error

--- service/loggingService.js
+++ service/loggingService.js
@@ -4,19 +4,19 @@ import { getParameter } from './configService.js';
 const loggingService = {
     log: (...params) => {
         console.log(...params);
-        const message = params.join(" ");
+        const message = JSON.stringify(params);
         socketService.emit('log', {id: 'SYSTEM', message, timestamp : new Date()});
     },
 
     error: (...params) => {
         console.error(params);
-        const message = params.join(" ");
+        const message = JSON.stringify(params);
         socketService.emit('log', {id: 'SYSTEM-ERROR', message, timestamp : new Date()});
     },
 
     debug: (...params) => {
         const debug = getParameter("DEBUG");
-        const message = params.join(" ");
+        const message = JSON.stringify(params);
         if (debug && debug.toLowerCase() == 'true'){
             console.log(...params);
         }
