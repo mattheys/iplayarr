9abe856012bfd2c9a5257e54fb3abfa4ae6dbc28
nikorag <nikorag@gmail.com>
2025-02-27 16:06:35 +0000

    Final Branding

--- frontend/src/views/AboutPage.vue
+++ frontend/src/views/AboutPage.vue
@@ -4,8 +4,12 @@
         <h3>iPlayarr</h3>
         <p>2025</p>
         <p>iPlayarr is a companion for Sonarr (and the rest of the *arr stack) to simplify integrating get_iplayer for episode search and download.</p>
-        <p><b>Author:</b></p>
+        <h2>Author</h2>
         <p><a href="https://github.com/Nikorag">Nikorag</a></p>
+        <h2>Credit To</h2>
+        <p><b>Phil Lewis</b> for development of get_iplayer</p>
+        <p>The Entire Development team behind Sonarr</p>
+        <p>The 2024 (World Champion) Philadelphia Eagles</p>
     </div>
 </template>
 
