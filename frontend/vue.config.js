410df43cea673a31e4d6dfc3939957879a749322
Jamie Bartlett <nikorag@gmail.com>
2025-03-11 11:45:09 +0000

    Vue PWA

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,5 +7,12 @@ module.exports = defineConfig({
         additionalData: `@import "~@/assets/styles/variables.less";`
       }
     }
+  },
+  pwa: {
+    name: 'iPlayarr',
+    themeColor: '#202020',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
   }
-})
+});
